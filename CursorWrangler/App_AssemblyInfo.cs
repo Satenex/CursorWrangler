756b7809ddadff365af3cbba565c60ca5a6e61b8
Satenex <112253104+Satenex@users.noreply.github.com>
2026-03-16 12:05:57 +0700

    Update App_AssemblyInfo.cs

--- CursorWrangler/App_AssemblyInfo.cs
+++ CursorWrangler/App_AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
-mashka м2
+mashka м2444
